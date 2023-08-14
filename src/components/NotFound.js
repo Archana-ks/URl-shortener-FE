4b6e9ae9fbdcd8bb297eef8c2dba2a3c016278bd
Archana-ks <achu24ks@gmail.com>
2023-08-14 21:39:31 +0530

    first commit

---
+++ src/components/NotFound.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import pagenotfoundImage from "./pagenotfound.jpeg";
+
+export const NotFound = () => {
+    return (
+      <div className="notFound">
+        <div className="pageNotFound">
+          <Link to="/Login">Go back to login page</Link>
+          <br />
+          <br />
+          <h1>Oops..! 404 Page Not Found</h1>
+           <img
+            className="imageForNotFound"
+            height="500"
+            width="500"
+            src={pagenotfoundImage}
+            alt="not found"
+          />
+        </div>
+      </div>
+    );
+  };
+  
